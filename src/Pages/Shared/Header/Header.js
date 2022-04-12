f7b16818ae5ccaab72172a4c94a2a4ee6d9a2a35
kridi32 <kridi32@outlook.com>
2022-04-12 22:33:45 +0600

    navbar is now responsive

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,18 +1,40 @@
 import React from "react";
-import { Nav, Container, Navbar } from "react-bootstrap";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <Navbar bg="success" variant="dark" fixed="top">
-      <Container>
-        <Navbar.Brand href="/">Rusty Bike</Navbar.Brand>
-        <Nav className="ms-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#services">Services</Nav.Link>
-          <Nav.Link href="#experts">Experts</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-success lg-fixed-top">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          Brandname
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
